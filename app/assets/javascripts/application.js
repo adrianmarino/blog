ce9338ed3a1db57db316154ac90880dcdf0d1a32
Adrian Marino <adrianmarino@gmail.com>
2015-01-11 12:30:55 -0300

    Blog with routing

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,15 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require underscore
+//= require angular
+//= require angular-route
+//= require_self
+//= require_tree ./controllers/global
+//= require_tree ./controllers/main
+//= require_tree ./directives/global
+//= require_tree ./directives/main
+//= require_tree ./filters/global
+//= require_tree ./filters/main
+//= require_tree ./services/global
+//= require_tree ./services/main
